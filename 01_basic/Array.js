42fbdcf2c216a487ee4e3a06021cdb61a4c5bb5d
JAYVIR KUMAR <kumarjayvir87@gmail.com>
2025-09-09 22:42:57 +0000

    Array commit

---
+++ 01_basic/Array.js
@@ -0,0 +1,15 @@
+// Creating Array in java scripts
+// let fruits = ["Apple","Banana","Mango","Orange"]
+// console.log(fruits)
+// // print element using index
+// console.log(fruits[0]) //Apple
+// console.log(fruits[1]) //Banana
+// console.log(fruits[2]) //Mango
+// console.log(fruits[3]) //Orange
+// console.log(fruits.length)  //print length of array
+
+//Array traversal 
+let arr = [4,5,8,68,75,6]
+for(let i = 0; i < arr.length; i++){
+
+}
