7ed1e3be02425d35dba6c6af8840bd2248a76d56
JAYVIR KUMAR <kumarjayvir87@gmail.com>
2025-09-17 20:50:12 +0000

    ertys

--- 01_basic/ArrayPracticsSet.js
+++ 01_basic/ArrayPracticsSet.js
@@ -49,60 +49,70 @@
 
 // 10. Extract ["Banana","Mango"] from ["Apple","Banana","Mango","Orange"] using slice().
 
-let fruit = ["Apple","Banana","Mango","Orange"];
-fruit.slice("Banana","Mango");
-console.log(fruit);
+// let fruit = ["Apple","Banana","Mango","Orange"];
+// fruit.slice("Banana","Mango");
+// console.log(fruit);
 
 
 
 // 🟡 Medium Level (Methods & Transformation)
 
 // 1. Double every number in [1,2,3,4] using map().
-let num = [1,2,3,4];
+// let num = [1,2,3,4];
 // num.map(n => n*2);
-console.log(num.map(n => n*2));
+// console.log(num.map(n => n*2));
 
 // 2. Filter out even numbers from [1,2,3,4,5,6].
-let num1 = [1,2,3,4,5,6];
+// let num1 = [1,2,3,4,5,6];
 // num1.filter(n => n%2 === 0);
-console.log(num1.filter(n => n%2 === 0));
+// console.log(num1.filter(n => n%2 === 0));
 
 // 3. Find the sum of [5,10,15,20] using reduce().
-let num2 = [5,10,15,20];
+// let num2 = [5,10,15,20];
 // num2.reduce();
-console.log(num2.reduce((a,b) => a+b));
+// console.log(num2.reduce((a,b) => a+b));
 
 // 4. Check if all numbers in [10,20,30,40] are greater than 5 using every().
-let num3 = [10,20,30,40];
+// let num3 = [10,20,30,40];
 // num3.every(n => n > 5);
-console.log(num3.every(n => n > 5));
+// console.log(num3.every(n => n > 5));
 
 // 5. Check if any number is less than 0 in [1,2,-3,4] using some().
-let num4 = [1,2,-3,4];
+// let num4 = [1,2,-3,4];
 // num4.every(n => n < 0);
-console.log(num4.some(n => n < 0));
+// console.log(num4.some(n => n < 0));
 
 // 6. Find the first number greater than 50 in [10,25,60,80].
 
 
 // 7. Find the index of "Banana" in ["Apple","Banana","Mango"].
 
+// let arr = ["Apple","Banana","Mango"];
+// console.log(arr.indexOf("Banana"));
 
 // 8. Sort the array [40,100,1,5,25] in ascending order.
 
+// let num = [40,100,1,5,25];
+// console.log(num.sort((a,b) => a-b));
 
 // 9. Reverse the array ["One","Two","Three"].
+// let re = ["One","Two","Three"];
+// console.log(re.reverse());
 
+// 10. Convert string "hello" into an array of characters using Array.from()
 
-// 10. Convert string "hello" into an array of characters using Array.fro
+// let arr1 = ["Hello","world"];
+// console.log(arr1.form("Hello"));
 
 // 🔴 Hard Level (Advanced & Real Use)
 
 // 1. Flatten the nested array [1,[2,[3,[4]]]] into [1,2,3,4].
-
+// let nums = [1,[2,[3,[4]]]];
+// console.log(nums.flat(3));
 
 // 2. Create a 2D array matrix = [[1,2,3],[4,5,6],[7,8,9]] and print 5.
-
+// let matrix = [[1,2,3],[4,5,6],[7,8,9]];
+// console.log(matrix[1][1]);
 
 // 3. Use destructuring to extract the first two elements from [100,200,300,400].
 
